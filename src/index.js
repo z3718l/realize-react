cdcf024338d9bff18166c14d7b7a27f2ffb369a8
张龙 <zhanglong@weipaitang.com>
2022-02-26 11:24:48 +0800

    feat: 初始化

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let jsxDom = <div>hello world</div>;
+
+ReactDOM.render(jsxDom, document.getElementById("root"));
